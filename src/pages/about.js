60f2a06f2bbe84d429a0abf28aaa35109476cd23
Blake D. Doty <blakester609@MacBook-Pro.local>
2023-08-11 13:04:18 -0600

    First commit

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import * as React from 'react'
+
+const AboutPage = () => {
+    return (
+        <main>
+            <h1>About Me</h1>
+            <h3>My name is Blake Doty</h3>
+            <p>I love doing almost anything outdoors. I especially love skiing and riding my bike. 
+                I also love dance. I enjoy discussing deep and interesting topics in science, religion, and life in general.</p>
+        </main>
+    )
+}
+
+export default AboutPage
