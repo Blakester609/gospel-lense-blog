481827566b0a63797e897fecbe009091c9b58c30
Blake D. Doty <blakester609@MacBook-Pro.local>
2023-08-11 12:00:52 -0600

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Gospel Lense Blog`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
