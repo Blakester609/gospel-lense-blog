bfbdf74f5de00c8c802ca2fe2d39966ee41cd52e
Blake D. Doty <blakester609@MacBook-Pro.local>
2023-08-11 13:45:33 -0600

    Second commit

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,14 +1,17 @@
 import * as React from 'react'
+import Layout from '../components/layout'
 
 const AboutPage = () => {
     return (
-        <main>
-            <h1>About Me</h1>
+        <Layout pageTitle="About Me">
             <h3>My name is Blake Doty</h3>
             <p>I love doing almost anything outdoors. I especially love skiing and riding my bike. 
                 I also love dance. I enjoy discussing deep and interesting topics in science, religion, and life in general.</p>
-        </main>
+        </Layout>
+        
     )
 }
+
+export const Head = () => <title>About Me</title>
 
 export default AboutPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
 import * as React from "react"
+import Layout from '../components/layout'
 
 const IndexPage = () => {
   return (
-    <main>
-      <h1>The Gospel Lense</h1>
-      <p>I'm making this by following the Gatsby Tutorial.</p>
-    </main>
+    <Layout pageTitle="Home Page">
+        <h1>The Gospel Lense</h1>
+        <p>I'm making this by following the Gatsby Tutorial.</p>
+    </Layout>
   )
 }
 
