1888aa0624402b837525333cef244d6741f75a57
Blake D. Doty <blakester609@MacBook-Pro.local>
2023-08-16 22:51:01 -0600

    Added some more styles to the layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,15 +1,9 @@
 import * as React from 'react'
 import { Link, useStaticQuery, graphql } from 'gatsby'
-import {
-    container,
-    heading,
-    navLinks,
-    navLinkItem,
-    navLinkText,
-    siteTitle
-} from './layout.module.css'
+import * as bootstrapBlog from './layout.module.css'
 
 const Layout = ({ pageTitle, children }) => {
+
     const data = useStaticQuery(graphql`
         query {
             site {
@@ -21,21 +15,30 @@ const Layout = ({ pageTitle, children }) => {
     `)
     
     return (
-        <div className={container}>
-            <title>Howdy</title>
-            <header className={siteTitle}>{data.site.siteMetadata.title}</header>
-            <nav>
-                <ul className={navLinks}>
-                    <li className={navLinkItem}><Link to="/" className={navLinkText}>Home</Link></li>
-                    <li className={navLinkItem}><Link to="/about" className={navLinkText}>About</Link></li>
-                    <li className={navLinkItem}><Link to="/blog" className={navLinkText}>Blog</Link></li>
-                </ul>
-            </nav>
-            <main>
-                <h1 className={heading}>{pageTitle}</h1>
-                {children}
-            </main>
-        </div>
+        <body>
+            <div>
+                <nav className={`${bootstrapBlog.navbar} ${bootstrapBlog.navbarExpandLg} ${bootstrapBlog.navbarLight}`}>
+                    <div className={`${bootstrapBlog.container} ${bootstrapBlog.px4} ${bootstrapBlog.pxLg5}`}>
+                        <Link to="/" className={bootstrapBlog.navbarBrand}>The Gospel Lens</Link>
+                        <div className={`${bootstrapBlog.collapse} ${bootstrapBlog.navbarCollapse}`} id={bootstrapBlog.navbarResponsive}>
+                            <ul className={`${bootstrapBlog.navbarNav} ${bootstrapBlog.msAuto} ${bootstrapBlog.py4} ${bootstrapBlog.pyLg0}`}>
+                                <li className={bootstrapBlog.navItem}><Link to="/" className={`${bootstrapBlog.navLink} ${bootstrapBlog.pxLg3} ${bootstrapBlog.py3} ${bootstrapBlog.pyLg4}`}>Home</Link></li>
+                                <li className={bootstrapBlog.navItem}><Link to="/about" className={`${bootstrapBlog.navLink} ${bootstrapBlog.pxLg3} ${bootstrapBlog.py3} ${bootstrapBlog.pyLg4}`}>About</Link></li>
+                                <li className={bootstrapBlog.navItem}><Link to="/blog" className={`${bootstrapBlog.navLink} ${bootstrapBlog.pxLg3} ${bootstrapBlog.py3} ${bootstrapBlog.pyLg4}`}>Blog</Link></li>
+                            </ul>
+                        </div>  
+                    </div>
+                </nav>
+                <header className={bootstrapBlog.masthead}>
+                    {data.site.siteMetadata.title}
+                </header>
+                <main>
+                    <h1>{pageTitle}</h1>
+                    {children}
+                </main>
+            </div>
+        </body>
+        
     )
 }
 
